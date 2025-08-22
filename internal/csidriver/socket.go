1c089420ee5fe35f55fc011b60b15bfff0c4fa14
Pontus Freyhult <pontus_github@soua.net>
2025-08-22 10:13:37 +0200

    Lint fix and some cleanup

--- internal/csidriver/socket.go
+++ internal/csidriver/socket.go
@@ -6,17 +6,27 @@ import (
 	"net"
 	"os"
 	"strings"
-	"time"
 )
 
 func CheckSocket(s *string) (bool, error) {
 
+	network := "unix"
+	var address string
+
 	endpointParts := strings.Split(*s, ":")
-	if endpointParts[0] != "unix" {
+
+	if len(endpointParts) > 1 {
+		network = endpointParts[0]
+		address = endpointParts[1]
+	} else {
+		address = endpointParts[0]
+	}
+
+	if network != "unix" {
 		return true, nil
 	}
 
-	sockStat, err := os.Stat(endpointParts[1])
+	sockStat, err := os.Stat(address)
 
 	if err != nil {
 		// Treat any error for stat as non-existant and fine
@@ -25,27 +35,21 @@ func CheckSocket(s *string) (bool, error) {
 
 	if fs.ModeSocket == (sockStat.Mode() & fs.ModeSocket) {
 		// Socket
-		_, err = net.Dial("unix", endpointParts[1])
+		_, err = net.Dial("unix", address)
 		if err == nil {
 			// Something working, don't start a new instance
 			return false, nil
 		}
 	} else {
-		return false, fmt.Errorf("%s exists but is not a leftover socker", endpointParts[1])
+		return false, fmt.Errorf("%s exists but is not a leftover socket", address)
 	}
 
 	// If we get here, there's a socket that doesn't work, so we remove that
 
-	err = os.Remove(endpointParts[1])
+	err = os.Remove(address)
 	if err != nil {
-		return false, fmt.Errorf("Couldn't remove stale endpoint %s: %v", endpointParts[1], err)
+		return false, fmt.Errorf("couldn't remove stale endpoint %s: %v", address, err)
 	}
-
-	// FIXME: Better way?
-	// Give kubernetes a chance to pick up that the old plugin is gone by waiting somewhat after removing the socket
-	// not sure why it's needed...
-
-	time.Sleep(1 * time.Second)
 
 	return true, nil
 
